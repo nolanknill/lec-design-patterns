d7e6b5821317ba99da90a744ecb29ecccc573178
Nolan Knill <nolanknill@gmail.com>
2022-08-30 15:36:38 -0400

    Added views for /contestants and /contestants/:contestantId pages

--- controllers/contestantController.js
+++ controllers/contestantController.js
@@ -3,14 +3,15 @@ const contestantModel = require("../models/contestantModel");
 const getContestants = (_req, res) => {
     const contestants = contestantModel.getAll();
 
-    res.json(contestants);
+    // res.json(contestants);
+    res.render("contestants", { contestants: contestants, title: "Rupaul's Drag Race Contestants" });
 };
 
 const getContestant = (req, res) => {
     const requestedId = Number(req.params.contestantId);
     const singleContestant = contestantModel.getOne(requestedId);
 
-    res.json(singleContestant);
+    res.render("contestant", { contestant: singleContestant});
 }
 
 module.exports = { getContestants, getContestant };
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8000;
 const contestantRoutes = require("./routes/contestantRoutes");
 
+app.set("view engine", "ejs");
+
 // This middleware allows us to post JSON in request.body
 app.use(express.json());
 
